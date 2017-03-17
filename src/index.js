019c701a2d4884dd29a6fc14976ee050f40d3b88
alvare48 <alvare48@purdue.edu>
2017-03-17 13:56:59 -0400

    "Updated screen paths"

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { Router, Route, browserHistory } from 'react-router';
 import LoginScreen from './js/screens/LoginScreen.jsx';
 import HomeScreen from './js/screens/HomeScreen.jsx';
 import EventScreen from './js/screens/EventScreen.jsx';
-import * as firebase from 'firebase';
+import SignUpScreen from './js/screens/SignUpScreen.jsx';
+//import * as firebase from 'firebase';
 
 // webpack-dev-server --progress --inline
 // Bootstrap
@@ -13,20 +14,21 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import './css/index.css';
 
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyBpBEVFlT7PpcPq3ZA_Yj0U6Lq1vQfvq0c",
   authDomain: "dazzling-inferno-9963.firebaseapp.com",
   databaseURL: "https://dazzling-inferno-9963.firebaseio.com",
   storageBucket: "gs://dazzling-inferno-9963.appspot.com",
 };
-const firebaseApp = firebase.initializeApp(firebaseConfig, 'MainFirebase');
+const firebaseApp = firebase.initializeApp(firebaseConfig, 'MainFirebase');*/
 
 ReactDOM.render(
 	<div>
 		<Router history={browserHistory}>
-			<Route path="/" component={LoginScreen} />
-		  <Route path="/event" component={EventScreen} />
-			<Route path="/home" component={HomeScreen} />
+			<Route path="/" component={LoginScreen}/>
+			<Route path="/event" component={EventScreen}/>
+			<Route path="/home" component={HomeScreen}/>
+			<Route path="/signup" component={SignUpScreen}/>
 		</Router>
 	</div>,
   document.getElementById('app')
